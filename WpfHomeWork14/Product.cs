6c585027eafce3fd5cd4edaad2407f7a5efff6d4
Dmitriy Larionov <larionov.d.y@gmail.com>
2022-01-17 22:22:32 +0300

    Добавьте файлы проекта.

---
+++ WpfHomeWork14/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHomeWork14
+{
+    public enum ProductCategory
+    {
+        Applience,
+        Food,
+        Meds
+    }
+    class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public ProductCategory Category { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
